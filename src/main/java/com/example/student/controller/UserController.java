cd8fa2ef7f694c7e54c872b64a0a7dd3dc3a60ae
lhl <718560214@qq.com>
2022-11-20 11:56:17 +0800

    模板抽取完成

--- src/main/java/com/example/student/controller/UserController.java
+++ src/main/java/com/example/student/controller/UserController.java
@@ -2,12 +2,27 @@ package com.example.student.controller;
 
 import com.example.student.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
+    @GetMapping("/index")
+    public String index(){
+        return "/tables";
+    }
+
+
+    @GetMapping("/test")
+    public String test(Model model){
+        return "/user_list";
+    }
 }
